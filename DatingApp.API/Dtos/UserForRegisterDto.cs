5f7a566f0da3a0d4e3ef98e1305b59275710f757
unknown <marco.puga@gmail.com>
2019-03-10 23:19:38 -0300

    Fin de seccion 3 Seguridad

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Debes especificar un password entre 4 y 8 caracteres")]
+        public string Password { get; set; }
+    }
+}
