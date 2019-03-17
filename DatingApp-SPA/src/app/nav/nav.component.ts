14c81cf4d3f2b4d4575ab1c658d77cf06888f579
unknown <marco.puga@gmail.com>
2019-03-17 01:31:07 -0300

    Fin de seccion 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logueado exitosamente');
     }, error => {
-      console.log('Login erroneo');
+      console.log(error);
     });
   }
 
